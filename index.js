beeaaa261c03b7d9d987b7913c18634239cc6224
natsumeaurlia <moto718program@gmail.com>
2022-03-28 00:52:47 +0900

    install husky and set lint command

---
+++ index.js
@@ -0,0 +1,4 @@
+//   1:1   error  Unexpected var, use let or const instead       no-var
+//   1:5   error  'hoge' is assigned a value but never used      no-unused-vars
+//   1:15  error  Newline required at end of file but not found  eol-last
+// var hoge = '';
